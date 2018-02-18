eb8e7bc3a3b8e32b786bcfa13db1ba26d477d6c5
Shikhar Jaiswal <jaiswalshikhar87@gmail.com>
2018-02-18 20:35:50 +0530

    Update

--- src/main.py
+++ src/main.py
@@ -1,20 +1,2 @@
 import pandas
 import matplotlib
-
-
-class SingularValueDecomposition(Basic):
-
-	def __init__(self):
-		super(SingularValueDecomposition, self).__init__()
-
-	def predict(self, u, i):
-		return None
-
-
-class RestrictedBoltzmannMachines(Basic):
-
-	def __init__(self):
-		super(RestrictedBoltzmannMachines, self).__init__()
-
-	def predict(self, u, i):
-		return None
