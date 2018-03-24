6fb344e91b1525c36e46c2325ab41cbd10a6cbc2
Shikhar Jaiswal <jaiswalshikhar87@gmail.com>
2018-03-24 18:02:41 +0530

    Update Testing

--- src/main.py
+++ src/main.py
@@ -1,2 +1,49 @@
+import numpy
 import pandas
 import matplotlib
+from basic import Basic
+from baseline import Baseline
+
+
+print("------------------Beginning Training and Testing Phase-----------------")
+
+try:
+	with open('../MovieLens 10M Dataset/train.csv', 'r') as a:
+		train = pandas.read_csv(a)
+		train = train.drop('timestamp', 1)
+	a.close()
+
+	X = train.values
+	Y = train['rating'].values
+	X = numpy.delete(X, 2, axis=1)
+
+	with open('../MovieLens 10M Dataset/test.csv', 'r') as b:
+		test = pandas.read_csv(b)
+		test = test.drop('timestamp', 1)
+	b.close()
+
+	X_test = test.values
+	Y_test = test['rating'].values
+	X_test = numpy.delete(X_test, 2, axis=1)
+except:
+	print("------------------------Failed To Load Dataset------------------------")
+	exit()
+else:
+	print("----------------------------Datasets Loaded----------------------------")
+
+
+print("-----------------------Baseline Predictor Testing----------------------")
+
+space = (numpy.linspace(1, 101, 100)).astype(int)
+scores = []
+
+model = Baseline()
+model.preprocess(X, Y)
+for k in space:
+	print("Epoch: %i", k)
+	Y_pred = [model.predict_baseline(i[0], i[1], k) for i in X_test]
+	score = numpy.sqrt(numpy.mean((Y_test - Y_pred)**2))
+	scores.append(score)
+
+matplotlib.plot(space, scores, 'ro')
+plt.show()
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,16 +1,30 @@
-with open('/home/shikhar/Movie-Recommendation/MovieLens 26M Dataset/ratings.csv', 'r') as a, open('/home/shikhar/Movie-Recommendation/MovieLens 26M Dataset/train.csv', 'w') as b, open('/home/shikhar/Movie-Recommendation/MovieLens 26M Dataset/test.csv', 'w') as c:
-	csvfile = a.readlines()
-	b.write(csvfile[0])
-	c.write(csvfile[0])
-	i = 1
-	while i < len(csvfile):
-		if csvfile[i][0] == csvfile[i + 4][0]:
-			b.write(csvfile[i])
-			b.write(csvfile[i + 1])
-			c.write(csvfile[i + 2])
-			b.write(csvfile[i + 3])
-			b.write(csvfile[i + 4])
-			i += 4
-		else:
-			b.write(csvfile[i])
-		i += 1
+#!/usr/bin/python
+
+
+print("--------------------------Pre-Processing Data--------------------------")
+
+try:
+	with open('../MovieLens 10M Dataset/ratings.csv', 'r') as a, open('../MovieLens 10M Dataset/train.csv', 'w') as b, open('../MovieLens 10M Dataset/test.csv', 'w') as c:
+		csvfile = a.readlines()
+		b.write(csvfile[0])
+		c.write(csvfile[0])
+		i = 1
+		while i < len(csvfile):
+			if csvfile[i][0] == csvfile[i + 4][0]:
+				b.write(csvfile[i])
+				b.write(csvfile[i + 1])
+				c.write(csvfile[i + 2])
+				b.write(csvfile[i + 3])
+				b.write(csvfile[i + 4])
+				i += 4
+			else:
+				b.write(csvfile[i])
+			i += 1
+	a.close()
+	b.close()
+	c.close()
+except:
+	print("-----------------------Failed To Prepare Dataset-----------------------")
+	exit()
+else:
+	print("---------------Train and Test Data Prepared Successfully---------------")
